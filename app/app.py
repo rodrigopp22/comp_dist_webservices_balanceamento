2dc58c89de39f541fa61753ecc54551650a3444b
Gabriel Tardochi Salles <60230715+ga-tardochisalles@users.noreply.github.com>
2021-04-20 20:43:27 -0300

    adicionado .gitignore e api flask finalizada

---
+++ app/app.py
@@ -0,0 +1,30 @@
+import requests
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/<valor_consultado_brl>', methods=['GET'])
+def home(valor_consultado_brl):
+    try:
+        valor_consultado_brl = float(valor_consultado_brl.replace(',','.'))
+    except:
+        return "Informar valor numerico.."
+    return convertemoeda(valor_consultado_brl)
+
+def convertemoeda(valor_em_brl):
+    response = requests.request('GET', 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL').json()
+    usd_para_real = float(response['USDBRL']['ask'])
+    eur_para_real = float(response['EURBRL']['ask'])
+    resultado = {
+        "conversao": {
+            "real": valor_em_brl,
+            "dolar": valor_em_brl/usd_para_real,
+            "euro": valor_em_brl/eur_para_real,
+        }
+    }
+    return jsonify(resultado)
+
+
+if __name__ == "__main__":
+    app.run()
