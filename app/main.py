cbc14df2708991eb82128b3cee66cb1e476a113b
rodrigopp22 <digoppasquale@msn.com>
2021-04-20 19:05:43 -0300

    Commit inicial.

--- app/main.py
+++ app/main.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 @app.route('/https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL', methods =['GET'])
 def getConversao():
     json_data = request.json
-    print(json_data)
+    return json_data
+    #print(json_data)
 
 if __name__ == '__main__':
     app.run()
+
+#https://www.kite.com/python/answers/how-to-get-json-from-a-request-using-flask-in-python
